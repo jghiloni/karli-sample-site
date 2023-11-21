3fe9a9df555b379c9d8fe6a25e1ded3e596b1ab6
Josh Ghiloni <ghiloni@gmail.com>
2023-11-20 19:54:54 -0500

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './IMG_5325.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is Mouse. There are many like him, but this one is ours. ❤️
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
